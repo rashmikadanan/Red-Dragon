4bb99aec3e992fced9d24a3c671526e60e843879
rashmikadanan <87791463+rashmikadanan@users.noreply.github.com>
2022-07-05 20:39:18 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,22 +20,22 @@ global.APIKeys = {
 //customize settings\\
 
 global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+My Creator is 1zero (KN16H7)😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94723128577'] //Owner number in aive msg
+global.premium = ['+94723128577'] //Owner Number info
+global.ownernomer = '+94723128577' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
+global.ownername = '1zero (KN16H7)😎' //Owner Name
 global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
 
 global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
+global.btnurl = 'https://www.youtube.com/c/easyguideLKRashmikaDananjaya' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
 global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.author = '1zero (KN16H7)😎' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
@@ -56,7 +56,7 @@ global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
     bot: 'This Feature Is Only For Bot😝',
     wait: '👉👈Pleas Wait...',
     error: 'Error!!! Maybe Api Key Is Expired🤔!',
-    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours subscribe on youtube https://www.youtube.com/c/easyguideLKRashmikaDananjaya',
 }
 // DO NOT CHANGE \\
     global.limitawal = {
